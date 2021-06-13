83c1bd4af9892fda194e8e6868de322265a0572b
SiddharthSemani <siddharthsemani@gmail.com>
2021-06-13 12:22:04 +0530

    Icon added and values of zoom and polyline changed

--- app/src/main/java/com/androiddevs/run_tracker/other/Constants.kt
+++ app/src/main/java/com/androiddevs/run_tracker/other/Constants.kt
@@ -24,8 +24,8 @@ object Constants {
     const val FASTEST_LOCATION_INTERVAL = 2000L
 
     const val POLYLINE_COLOR = Color.RED
-    const val POLYLINE_WIDTH = 8f
-    const val MAP_ZOOM = 15f
+    const val POLYLINE_WIDTH = 12f
+    const val MAP_ZOOM = 20f
 
     const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
     const val NOTIFICATION_CHANNEL_NAME = "Tracking"
